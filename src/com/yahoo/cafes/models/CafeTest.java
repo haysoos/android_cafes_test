a0b2226ef7881b2ad860b681dbb19b77d91a7722
jmedrano <jmedrano@yahoo-inc.com>
2013-10-30 21:09:10 -0700

    Added Cafe unit tests.

--- src/com/yahoo/cafes/models/CafeTest.java
+++ src/com/yahoo/cafes/models/CafeTest.java
@@ -1,6 +1,7 @@
 package com.yahoo.cafes.models;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.fail;
 
 import java.io.File;
@@ -21,11 +22,7 @@ public class CafeTest {
 	@Before
 	public void setUp() throws Exception {
 		cafe = Cafe.getInstance();
-	}
-
-	@Test
-	public final void testCafeLoadFromJson() {
-		Assert.assertEquals(0, cafe.getNumberOfLocations());
+		
 		File file = new File("assets/example_all.json");
 		System.out.println(file.exists() + " " + file.getAbsolutePath());
 		JSONArray jsonArray;
@@ -34,13 +31,43 @@ public class CafeTest {
 			jsonString = FileUtils.readFileToString(file);
 			jsonArray = new JSONArray(jsonString);
 			cafe.loadLocations(jsonArray);
-			assertEquals(9, cafe.getNumberOfLocations());
-			assertEquals(9, cafe.getLocations().size());
+			
 		} catch (JSONException e) {
 			fail("INVALID JSON ARRAY STRING\n\n" + jsonString + "\n\n" + e.getMessage());
 		} catch (IOException e) {
 			fail("INVALID JSON ARRAY STRING\n\n" + jsonString + "\n\n" + e.getMessage());
 		}
+	}
+
+	@Test
+	public final void testCafeLoadFromJson() {
+		assertEquals(9, cafe.getNumberOfLocations());
+		assertEquals(9, cafe.getLocations().size());
+	}
+	
+	@Test
+	public final void testGetMenuItemById() {
+		MenuItem menuItem = cafe.getMenuItemById(959);
+		assertEquals(612, menuItem.getMenuId());
+		assertEquals(959, menuItem.getMenuItemId());
+		assertEquals("paneer makhani", menuItem.getTitle());	
+	}
+	
+	@Test
+	public final void testMyComment() {
+		MenuItem menuItem = cafe.getMenuItemById(959);
+		Comment myComment = new Comment();
+		myComment.setCommentId(123);
+		myComment.setMenuItemId(menuItem.getMenuItemId());
+		myComment.setText("My Comment");
+		myComment.setUserId(1234);
+		myComment.setUsername("someuser@example.com");
+		myComment.setUserRating(4);
+		menuItem.addComment(myComment);
+		menuItem.setMyComment(myComment);
+
+		assertNotNull(cafe.getMenuItemById(959).getMyComment());
+		
 	}
 	
 }
